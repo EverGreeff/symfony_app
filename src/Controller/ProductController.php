7b4b3f1798b022c4c4fcebb65857a6e8dbefa66f
EverGreeff <everton.greeff@universo.univates.br>
2025-12-14 11:35:11 -0300

    Reading Records

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
 
 final class ProductController extends AbstractController
 {
@@ -19,5 +20,25 @@ final class ProductController extends AbstractController
         return $this->render('product/index.html.twig', [
             'products' => $repository->findAll(),
         ]);
+    }
+
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product, ProductRepository $repository): Response
+    {
+        #$product = $repository->findOneBy(['id' => $id]);
+
+        #if($product === null) {
+        #    throw $this->createNotFoundException('Product not found');
+        #}
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+
+    #[Route('product/new', name: 'product_new')]
+    public function new(): Response 
+    {
+        return $this->render('product/new.html.twig');
     }
 }
